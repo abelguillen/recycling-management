3a28f5108e8e01b4184bc1a762ee3fe8209de890
abelguillen <abel.guillen@hotmail.com>
2024-10-22 10:43:58 -0300

    Add ProductService implementation for Product entity
    Related to #2
    
    Add Javadoc comments to `ProductService` interface and `ProductServiceImpl` class.
    
    * **ProductService.java**
      - Add Javadoc comments for each method in the `ProductService` interface.
    * **ProductServiceImpl.java**
      - Add Javadoc comments for each method in the `ProductServiceImpl` class.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/abelguillen/recycling-management/issues/2?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/main/java/com/aguillen/recyclingmanagement/service/ProductService.java
+++ src/main/java/com/aguillen/recyclingmanagement/service/ProductService.java
@@ -6,13 +6,43 @@ import com.aguillen.recyclingmanagement.entity.Product;
 
 public interface ProductService {
 
-	Product create(Product product);
-	
-	Product getById(Long id);
-	
-	Product update(Long id, Product updatedProduct);
-	
-	List<Product> getAll();
-	
-	Product delete(Long id);
+    /**
+     * Creates a new product in the database.
+     *
+     * @param product The product to be created.
+     * @return The created product.
+     */
+    Product create(Product product);
+
+    /**
+     * Retrieves a product by its ID from the database.
+     *
+     * @param id The ID of the product to be retrieved.
+     * @return The retrieved product, or null if no product was found with the given ID.
+     */
+    Product getById(Long id);
+
+    /**
+     * Updates a product in the database.
+     *
+     * @param id The ID of the product to be updated.
+     * @param updatedProduct The product data to be updated.
+     * @return The updated product, or null if no product was found with the given ID.
+     */
+    Product update(Long id, Product updatedProduct);
+
+    /**
+     * Retrieves all products from the database.
+     *
+     * @return A list of all products.
+     */
+    List<Product> getAll();
+
+    /**
+     * Deletes a product by its ID from the database.
+     *
+     * @param id The ID of the product to be deleted.
+     * @return The deleted product, or null if no product was found with the given ID.
+     */
+    Product delete(Long id);
 }
